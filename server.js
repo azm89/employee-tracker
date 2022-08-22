e672a237557d20f76127f059e045c17ddbeff23b
azm89 <ashtonmoore89@gmail.com>
2022-08-22 15:59:23 -0700

    started server.js

---
+++ server.js
@@ -0,0 +1,86 @@
+const {prompt} = require('inquirer');
+const db = require('./db');
+
+init();
+
+function init() {
+    runPrompts();
+}
+
+function runPrompts() {
+    prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices: [
+                {
+                    name: "View All Departments",
+                    value: "VIEW_DEPARTMENTS"
+                },
+                {
+                    name: "View All Roles",
+                    value: "VIEW_ROLES"
+                },
+                {
+                    name: "View All Employees",
+                    value: "VIEW_EMPLOYEES"
+                },
+
+                {
+                    name: "Add a Department",
+                    value: "ADD_DEPARTMENT"
+                },
+                {
+                    name: "Add a Role",
+                    value: "ADD_ROLE"
+                },
+                {
+                    name: "Add an Employee",
+                    value: "ADD_EMPLOYEE"
+                },
+                {
+                    name: "Update Employee Role",
+                    value: "UPDATE_EMPLOYEE_ROLE"
+                },
+                {
+                    name: "Quit",
+                    value: "QUIT"
+                }
+            ]
+        }
+    ]).then(res => {
+        let choice = res.choice;
+        switch (choice) {
+            case "VIEW_DEPARTMENTS":
+                viewDepartments();
+                break;
+            case "VIEW_ROLES":
+                viewAllRoles();
+                break;
+            case "VIEW_EMPLOYEES":
+                viewAllEmployees();
+                break;
+            case "ADD_DEPARTMENT":
+                addDepartment();
+                break;
+            case "ADD_ROLE":
+                addRole();
+                break;
+            case "ADD_EMPLOYEE":
+                addEmployee();
+                break;
+            case "UPDATE_EMPLOYEE_ROLE":
+                updateEmployeeRole();
+                break;
+            default:
+                quit();
+        }
+    }
+    )
+}
+
+function viewAllEmployees() {
+    db.allEmployees()
+        .then(employees => {
+}
